2c4393b533f1e555259d6402f86335bfc758f92c
ferengere <ferengere@gmail.com>
2015-05-13 20:14:11 +0200

    modal nuevo valida

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,11 +4,21 @@
 var control = {
     _tablaDoctores: null,
     _validarDoctor: null,
-    _formDoctor: $('#formDoctor'),
-    _modalDoctor: $('#modalDoctor'),
+    _modalDoctor: null,
+    _formDoctor: null,
     _modalBorrar: $('#modalBorrar'),
+    _modalConfirmar: $('#modalConfirmar'),
     _idDoctor: null,
     _editarDoctor: false, //false nuevo, true editar
+    _msgConfirmar: {
+        nuevo: 'Se creará un nuevo doctor',
+        editar: 'Se modificarán los datos del doctor',
+        borrar: 'Se borrarán los datos del doctor'
+    },
+    _init: function() {
+        this._modalDoctor = $('#modalDoctor');
+        this._formDoctor = $('#formDoctor');
+    },
     formatList: function(data) {
         //poner nombres de clínicas en forma de lista en el campo de la tabla
         var lista = data.split(',');
@@ -47,7 +57,7 @@ var control = {
             self.formatSelect(clinicasDoctor, JSON.parse(data));
         });
 
-        $('#modalDoctor').modal('show');
+        //$('#modalDoctor').modal('show');
     },
     formatSelect: function(cliDoc, cliAll) {
         //crear el select con todas las clinicas, seleccionando las del doctor
@@ -84,38 +94,55 @@ var control = {
         return $.ajax({
             url: 'php/cargar_clinicas.php'
         });
+    },
+    borrarDoctor: function(id) {
+        return $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: 'php/borrar_doctor.php',
+            data: {
+                id: id
+            }
+        });
+    },
+    registroDoctor: function(data) {
+        //inserta o edita los datos de un doctor
+        return $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: 'php/registro_doctor.php',
+            data: {
+                editar: data.editar,
+                id: data.id,
+                nombre: data.nombre,
+                numero: data.numero,
+                clinicas: data.clinicas
+            }
+        });
+    },
+    mensajeGrowl: function(mensaje, tipo) {
+        $.bootstrapGrowl(mensaje, {
+            type: tipo,
+            offset: {
+                from: 'bottom',
+                amount: 20
+            }
+        });
+    },
+    confirmarOperacion: function() {
+
     }
 };
 
+/*var validarFormulario = function() {
 
-
-
-
-
-
-var borrarDoctor = function(id) {
-    return $.ajax({
-        type: 'POST',
-        dataType: 'json',
-        url: 'php/borrar_doctor.php',
-        data: {
-            id: id
-        }
-    });
-};
-
-
-var validarFormulario = function() {
-
-
-
-
-};
+};*/
 
 
 $(document).ready(
     function() {
         console.log('DOCUMENT.READY');
+        control._init();
 
         control._tablaDoctores = $('#tablaDoctores').DataTable({
             'processing': true,
@@ -189,12 +216,11 @@ $(document).ready(
             rules: {
                 nombre: {
                     required: true
-                        /*,
-                                            lettersonly: true*/
+
                 },
                 numero: {
                     digits: function() {
-                        if ($('#formDoctor #numero').val() !== '') {
+                        if ($('#numero').val() !== '') {
                             return true;
                         }
                     }
@@ -210,28 +236,121 @@ $(document).ready(
             messages: {
                 nombre: {
                     required: 'Debe escribir el nombre'
-                        /*,
-                                            lettersonly: 'Debe escribir sólo letras'*/
+
                 },
                 numero: {
                     digits: 'Debe escribir sólo números'
                 },
-                clinicas: {
+                'selClinicas[]': {
                     required: 'Debe seleccionar al menos una clínica'
                 }
+            },
+            submitHandler: function(form) {
+                console.log('VALIDAR FORMDOCTOR');
             }
-            /*,
-                        submitHandler: function() {
 
-                        }*/
         });
+
+        var validatorEditar = $('#editarDoctor').validate({
+            rules: {
+                nombreEditar: {
+                    required: true,
+                    minlength: 2,
+                    validaNombre: true, // modificado para que admita ñ, acentos, espacios ...
+                    maxlength: 100
+                },
+                numeroColegiadoEditar: {
+                    //  required: true,
+                    digits: true, // solo numeros pero no cifras (-12) no es válido
+                    minlength: 4,
+                    maxlength: 7
+                },
+                'seleccionaClinicasEditar[]': {
+                    required: true,
+                    minlength: 1
+                }
+            },
+            // unos cuantos mensajes personalizados
+            messages: {
+                numeroColegiadoEditar: {
+                    //  required: "Para modificar un doctor es necesario un numero de colegiado",
+                    digits: "El numero de colegiado solo puede tener numeros",
+                    minlength: "El número de colegiado debe tener al menos {0} digitos", // {0} es el valor del primer parametro 
+                    maxlength: "El número de colegiado debe tener como mucho {0} digitos"
+                },
+                nombreEditar: {
+                    required: "Para editar un doctor es necesario un nombre de doctor",
+                    minlength: "El nombre del doctor debe tener al menos {0} caracteres", // {0} es el valor del primer parametro
+                    maxlength: "El nombre del doctor no puede tener mas de {0} caracteres",
+                    validaNombre: "El nombre del doctor solo puede contener letras"
+                },
+                'seleccionaClinicasEditar[]': {
+                    required: "Cada doctor editado tiene que tener alguna clínica asignada",
+                    minlength: "El doctor editado debe tener al menos {0} clinica asignada",
+                }
+            },
+            submitHandler: function(form) {
+                console.log("en el boton  submitHandler botonConfirmarEditarDoctor");
+                /*var idDoctor = $("#idDoctorEditar").val();
+                var clinicas = $("#seleccionaClinicasEditar").val();
+                var nombre = $("#nombreEditar").val();
+                var numeroColegiado = $("#numeroColegiadoEditar").val();
+                $.ajax({
+                    type: 'POST',
+                    dataType: 'json',
+                    // un unico php para todas las acciones
+                    url: "php/mto_doctor.php",
+                    async: false,
+                    //estos son los datos que queremos usar, en json:
+                    data: {
+                        accion: 'editarDoctor', // la acción que ejecuto el php es un parámetro más
+                        idDoctor: idDoctor,
+                        clinicas: clinicas,
+                        nombre: nombre,
+                        numeroColegiado: numeroColegiado
+                    },
+                    error: function(xhr, status, error) {
+                        //el error se muestra con growl
+                        $.growl.error({
+                            message: "Error al editar un doctor!" + error
+                        });
+                    },
+                    success: function(data) {
+                        //obtenemos el mensaje del servidor, es un array!!!
+                        //var mensaje = (data["mensaje"]) //o data[0], en función del tipo de array!!
+                        var $mitabla = $("#miTabla").dataTable({
+                            bRetrieve: true
+                        });
+                        //actualizamos datatables:
+                        $mitabla.fnDraw();
+                        $.growl({
+                            title: "Exito!",
+                            // colocando el mensaje centrado arriba ... manias ...
+                            location: "tc",
+                            size: "large",
+                            style: "warning",
+                            message: "El doctor ha sido editado con exito"
+                        });
+                        $('#modal-editar').modal('hide');
+                    },
+                    complete: {
+                        //si queremos hacer algo al terminar la petición ajax
+
+                    }
+                });*/
+            }
+        });
+
+
+
 
         //evento pulsar boton nuevo
         $('.btnNuevo').on('click', function(e) {
             var evt = e || window.event;
-            evt.preventDefault();
-            console.log('Boton Crear Doctor pulsado');
+            //evt.preventDefault();
+            console.log('Boton Crear pulsado');
             control._editarDoctor = false;
+            control.mensajeGrowl('Añadir nuevo doctor', 'info');
             control.initForm($(this));
         });
 
@@ -242,29 +361,28 @@ $(document).ready(
             evt.preventDefault();
             console.log('Boton Editar pulsado');
             control._editarDoctor = true;
+            control.mensajeGrowl('Editar doctor', 'info');
             control.initForm($(this));
         });
 
-        /*$('#tablaDoctores').on('click', 'a', function(e) {
-            
-        });*/
 
         //evento pulsar boton borrar
         $('#tablaDoctores').on('click', '.btnBorrar', function(e) {
             var evt = e || window.event;
             evt.preventDefault();
             console.log('Boton Borrar pulsado');
+            control.borrarDoctor($(this));
 
-            var id = $(evt.target).attr('id');
+            /*var id = $(evt.target).attr('id');
             console.log(id);
 
 
             $('#modalBorrar').prop('idDoctor', id);
             $('#modalBorrar div.modal-body').html('<p>Desea borrar el doctor ' + id + ' ?</p>');
-            $('#modalBorrar').modal('show');
+            $('#modalBorrar').modal('show');*/
         });
 
-        $('#modalDoctor').on('click', '#btnConfirmarEditar', function(e) {
+        /*$('#modalDoctor').on('click', '#btnConfirmarEditar', function(e) {
             var evt = e || window.event;
             evt.preventDefault();
             console.log('Boton Confirmar Editar pulsado');
@@ -276,7 +394,22 @@ $(document).ready(
 
             //$('#modalDoctor').modal('hide');
 
-        });
+        });*/
+
+        /*$('#modalDoctor').on('click', '#btnConfirmarEditar', function(e) {
+            var evt = e || window.event;
+            //evt.preventDefault();
+            console.log('Boton Confirmar Editar pulsado');
+
+            if (control._validarDoctor.valid()) {
+                console.log('VALIDO');
+                control.formValido();
+                console.log('FIN VALIDO');
+            }
+
+
+
+        });*/
 
         $('#modalBorrar').on('click', '#btnConfirmarBorrar', function(e) {
             var evt = e || window.event;
@@ -286,7 +419,7 @@ $(document).ready(
 
             ///BORRAR DOCTOR
             var mensaje;
-            var promise = borrarDoctor(id);
+            var promise = control.borrarDoctor(id);
             promise.success(function(data) {
                 var res = data[0];
                 console.log(data);
@@ -310,11 +443,3 @@ $(document).ready(
 
 
     }); //ready
-
-
-
-
-
-/*$('div').on('click', function () {
-    console.log('li pulsado')
-});*/
